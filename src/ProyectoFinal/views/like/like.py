117f0fcd38ec89728d0ba66b3efebfce120a33b2
mauvanga <117782288+mauvanga@users.noreply.github.com>
2023-05-26 20:29:16 +0200

    Add files via upload

---
+++ src/ProyectoFinal/views/like/like.py
@@ -0,0 +1,58 @@
+import uuid
+
+import flask
+import sirope
+from flask import Blueprint, current_app, request, url_for, session
+from flask_login import login_required, logout_user
+
+from model.likedto import LikeDto
+from model.tuitdto import TuitDto
+from model.userdto import UserDto
+
+from views.tuit.tuit import buscartuits
+
+
+def get_blprint():
+ search = flask.blueprints.Blueprint("like", __name__,
+ url_prefix="/like",
+ template_folder="templates",
+ static_folder="static")
+
+ syrp = sirope.Sirope()
+
+ return search, syrp
+
+like_bp, srp = get_blprint()
+
+
+
+@like_bp.route('/increase_like', methods=['POST','GET'])
+def increase_like():
+    twit_id = request.form['twit_id']
+    usr = UserDto.current_user()
+    encontrado = 0
+
+    for tuit in srp.load_all_keys(TuitDto):
+        tuit_actual = srp.load(tuit)
+        if tuit_actual.id == twit_id:
+            for like in srp.load_all_keys(LikeDto):
+                like_actual = srp.load(like)
+                if like_actual.tuit == twit_id:
+                    if usr.email == like_actual.usr:
+                        tuit_actual.quitarLike()
+                        srp.save(tuit_actual)
+                        srp.delete(like)
+                        encontrado = 1
+            if(encontrado==0):
+                tuit_actual.darLike()
+                srp.save(tuit_actual)
+                srp.save(LikeDto(usr.email,twit_id))
+    if (request.referrer.endswith("/tuit/mistuits")):
+        return flask.redirect("/tuit/mistuits")
+    elif("/comentario/verComentarios" in request.referrer):
+        return flask.redirect(url_for('comentario.verComentarios', twit_id=twit_id))
+    elif ("/tuit/buscarTuits" in request.referrer):
+        palabra_clave = session.get('palabra_clave')
+        return flask.redirect(flask.url_for('tuit.buscartuits', edBusqueda=palabra_clave))
+    else:
+        return flask.redirect("/")
---
+++ src/ProyectoFinal/views/tuit/tuit.py
@@ -0,0 +1,104 @@
+import uuid
+
+import flask
+import sirope
+from flask import Blueprint, current_app, request, session
+
+from model.comentariodto import ComentarioDto
+from model.tuitdto import TuitDto
+from model.userdto import UserDto
+
+
+def get_blprint():
+ search = flask.blueprints.Blueprint("tuit", __name__,
+ url_prefix="/tuit",
+ template_folder="templates",
+ static_folder="static")
+
+ syrp = sirope.Sirope()
+
+ return search, syrp
+
+tuit_bp, srp = get_blprint()
+
+@tuit_bp.route('/', methods=["GET", "POST"])
+def get_index():
+     id = str(uuid.uuid4())
+     usr = UserDto.current_user()
+     # Recuperar y guardar el nuevo mensaje
+     if flask.request.method == "POST":
+        message_txt = flask.request.form.get("edMessage")
+        if not message_txt:
+            flask.flash("No puedes subir un tuit vacio.")
+            return flask.redirect("/")
+        if len(message_txt) > 280:
+            flask.flash("El tuit no puede superar los 280 caracteres.")
+            return flask.redirect("/")
+        srp.save(TuitDto(id,usr.email,message_txt))
+     # Visualizarlos todos
+     messages_list = list(srp.load_all(TuitDto))
+     comentarios = list(srp.load_all(ComentarioDto))
+
+     sust = {
+         "usr": usr,
+         "messages_list": messages_list,
+         "comentarios": comentarios,
+     }
+
+     return flask.render_template("home.html", **sust)
+
+
+@tuit_bp.route('/mistuits', methods=["GET", "POST"])
+def verMisTuits():
+    tuits = list(srp.load_all(TuitDto))
+    comentarios = list(srp.load_all(ComentarioDto))
+    mis_tuits = [tuit for tuit in tuits if tuit.usr == UserDto.current_user().email]
+    sust = {
+        "tuits": mis_tuits,
+        "comentarios": comentarios,
+    }
+
+    return flask.render_template("mistuits.html", **sust)
+
+@tuit_bp.route('/delete_twit', methods=['POST','GET','DELETE'])
+def delete_twit():
+    twit_id = request.form['twit_id']
+
+    for tuit in srp.load_all_keys(TuitDto):
+        tuit_actual = srp.load(tuit)
+        if tuit_actual.id == twit_id:
+            srp.delete(tuit)
+        else:
+            print("Elemento no encontrado")
+
+    if(request.referrer.endswith("/tuit/mistuits")):
+        return flask.redirect("/tuit/mistuits")
+    elif("/tuit/buscarTuits" in request.referrer):
+        palabra_clave = session.get('palabra_clave')
+        return flask.redirect(flask.url_for('tuit.buscartuits', edBusqueda=palabra_clave))
+    else:
+        return flask.redirect("/")
+
+
+@tuit_bp.route('/buscarTuits', methods=['POST', 'GET'])
+def buscartuits(edBusqueda=None):
+        edBusqueda = flask.request.args.get("edBusqueda") or flask.request.form.get("edBusqueda")
+        tuits_filtrados = filtrartuits(edBusqueda)
+        return flask.render_template('tuitsbuscados.html', tuits=tuits_filtrados)
+
+
+def filtrartuits(edBusqueda=None):
+    palabra_clave = edBusqueda
+
+    if not palabra_clave:
+        tuits_encontrados = list(srp.load_all(TuitDto))
+        session['palabra_clave'] = ''
+    else:
+        session['palabra_clave'] = palabra_clave
+        tuits_encontrados = []
+
+        for tuit in srp.load_all(TuitDto):
+            if palabra_clave.lower() in tuit.msg.lower():
+                tuits_encontrados.append(tuit)
+
+    return tuits_encontrados
